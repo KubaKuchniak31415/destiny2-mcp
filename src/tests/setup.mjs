b2c4de9d8e16fc48e03459009eecb09cb8d3008c
KubaKuchniak31415 <kubakuchniak31415@gmail.com>
2026-09-01 11:30:15 +0100

    Fixed the config dir issue for windows runner

--- src/tests/setup.mjs
+++ src/tests/setup.mjs
@@ -1,1 +1,6 @@
-process.env.XDG_CONFIG_HOME = './.tmp-test-config';
+const TEST_CONFIG_HOME = './.tmp-test-config';
+if (process.platform === 'win32') {
+  process.env.APPDATA = TEST_CONFIG_HOME;
+} else {
+  process.env.XDG_CONFIG_HOME = TEST_CONFIG_HOME;
+}
